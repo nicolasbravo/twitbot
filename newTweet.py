4fe60b700d9ffc0f7e6358e8bd60315bbeadda95
Nicolas Bravo <nicobravo1@hotmail.com>
2016-12-11 16:27:29 -0500

    Update newTweet.py

--- newTweet.py
+++ newTweet.py
@@ -9,6 +9,7 @@ totalWordsGenerator = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
 totalWords = random.choice(totalWordsGenerator)
 wordCounter = 1
 wordArray = array('l')
+tweetStr = ""
 
 words = 0
 
@@ -17,9 +18,10 @@ while wordCounter <= totalWords:
         words = words + 1
         wordCounter = wordCounter + 1
 #tweetStr = str(wordArray[1])
-print(wordArray[1])
+#print(wordArray[1])
 for l in wordArray:
-        if l <= totalWords - 1:
-                print(wordArray[l + 1])
+        tweetStr = tweetStr + wordArray[l]
+        #if l <= totalWords - 1:
+                #print(wordArray[l + 1])
                 #tweetStr = tweetStr + " " + str(wordArray[l + 1])
-#print(tweetStr)
+print(tweetStr)
