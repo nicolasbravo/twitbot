4eefd6fc21bbce568fc7ffa2c7b0ce9e02a8e4e8
Nicolas Bravo <nicobravo1@hotmail.com>
2016-12-11 15:01:00 -0500

    Update newTweet.py

--- newTweet.py
+++ newTweet.py
@@ -1,1 +1,16 @@
-#
+#!/usr/bin/env python
+import sys
+import string
+import random
+from twython import Twython
+from array import *
+
+totalWordsGenerator = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
+totalWords = random.choice(totalWordsGenerator)
+wordCounter = 1
+wordArray = array('l')
+
+while wordCounter <= totalWords:
+        wordArray.insert(words, for l in wordArray:
+        tweetStr = 
+#words = array('l', [1, 2, 3, 4, 5])
