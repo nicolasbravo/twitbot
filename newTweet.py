8de3977d9e700ba695c29d9ec5a5bc38f9cf68e9
Nicolas Bravo <nicobravo1@hotmail.com>
2016-12-11 17:52:11 -0500

    Replaced numbers with nouns
    Still need to update the rest of the plural nouns

--- newTweet.py
+++ newTweet.py
@@ -6,23 +6,31 @@ from twython import Twython
 from array import *
 
 totalWordsGenerator = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+countnGenerator = ['book', 'diary', 'laptop', 'movie', 'remote', 'shoe', 'bookbag', 'paper', 'clock', 'word', 'program', 'sheet', 'exam', 'sock']
+
 totalWords = random.choice(totalWordsGenerator)
 wordCounter = 1
 wordArray = array('l')
 tweetStr = ""
 
-words = 0
+#words = 0
 
 while wordCounter <= totalWords:
-        wordArray.append(words)
-        words = words + 1
+        wordGenerator = ['plural countn']
+        word = random.choice(wordGenerator)
+        if word == 'countn':
+                #count noun
+                countn = random.choice(countnGenerator)
+                wordArray.append(countn)
+        elif word == 'plural countn':
+                #count noun
+                countn = random.choice(countnGenerator)
+                #plural count noun
+                if countn[-1] == 's' or countn[-1] == 'x' or countn[-1] == 'z' or (countn[-1] == 'h' and countn[-2] == 'c') or (countn[-1] == 'h' and countn[-2] == 's'):
+                        pluralCountn = countn + 'es'
+                wordArray.append(pluralCountn)
         wordCounter = wordCounter + 1
-#tweetStr = str(wordArray[1])
-#print(wordArray[1])
 for l in wordArray:
         tweetStr = tweetStr + " " + str(wordArray[l])
-        #if l <= totalWords - 1:
-                #print(wordArray[l + 1])
-                #tweetStr = tweetStr + " " + str(wordArray[l + 1])
 tweetStr = tweetStr[1:]
 print(tweetStr)
