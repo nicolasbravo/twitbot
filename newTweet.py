10491454dfee4a06170a422bcc5e0f1cc77a5841
Nicolas Bravo <nicobravo1@hotmail.com>
2016-12-11 18:56:08 -0500

    Added the rest of the plural nouns

--- newTweet.py
+++ newTweet.py
@@ -30,6 +30,30 @@ while wordCounter <= totalWords:
                 #plural count noun
                 if countn[-1] == 's' or countn[-1] == 'x' or countn[-1] == 'z' or (countn[-1] == 'h' and countn[-2] == 'c') or (countn[-1] == 'h' and countn[-2] == 's'):
                         pluralCountn = countn + 'es'
+                elif countn[-1] == 'y' and countn[-2] != 'a' and countn[-2] != 'e' and countn[-2] != 'i' and countn[-2] != 'o' and countn[-2] != 'u':
+                        pluralCountn = countn[:-1] + 'ies'
+                elif countn[-1] == 'f' and ((countn[-2] == 'l' and countn[-3] == 'a') or (countn[-2] == 'l' and countn[-3] == 'e') or (countn[-2] == 'a' and countn[-3] == 'e')):
+                        pluralCountn = countn[:-1] + 'ves'
+                elif countn[-1] == 'e' and countn[-2] == 'f' and countn[-3] == 'i':
+                        pluralCountn = countn[:-2] + 'ves'
+                elif countn[-1] == 's' and countn[-2] == 'u':
+                        pluralCountn = countn[:-2] + 'i'
+                elif countn[-1] == 's' and countn[-2] == 'i':
+                        pluralCountn = countn[:-2] + 'es'
+                elif countn[-1] == 'x' and (countn[-2] == 'i' or countn[-2] == 'e'):
+                        pluralCountn = countn[:-2] + 'ices'
+                elif countn[-1] == 'u' and countn[-2] == 'a' and countn[-3] == 'e':
+                        pluralCountn = countn + 'x'
+                elif (countn[-1] == 'm' and countn[-2] == 'u') or (countn[-1] == 'n' and countn[-2] == 'o') and countn != 'person'
+                        pluralCountn = countn[:-2] + 'a'
+                elif countn == 'person':
+                        pluralCountn = 'people'
+                elif countn[1] == 'o' and countn[2] == 'o':
+                        pluralCountn = countn.replace('o', 'e', 2)
+                elif countn[-1] == 'a':
+                        pluralCountn = countn + 'e'
+                elif countn[-1] == 'e' and countn[-2] == 's' and countn[-3] == 'u' and countn[-4] == 'o':
+                        pluralCountn = countn[:-4] + 'ice'
                 else:
                         pluralCountn = countn + 's'
                 #wordArray.append(pluralCountn)
