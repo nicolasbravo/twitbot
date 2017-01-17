9f0a4900d8de466c5c0a2b3d1593cbac04a056a7
Nicolas Bravo <nicobravo1@hotmail.com>
2017-01-17 13:20:52 -0500

    New file to contain all of the words from newTweet.py

---
+++ words.py
@@ -0,0 +1,22 @@
+#noncount nouns
+ncountnGenerator = ['tennis', 'water', 'money', 'light', 'power', 'information', 'history', 'food', 'music', '2017', 'sleep', 'aesthetic', 'spam', 'cost', 'storage']
+#count nouns
+countnGenerator = ['book', 'diary', 'laptop', 'movie', 'remote', 'shoe', 'backpack', 'paper', 'clock', 'word', 'program', 'sheet', 'exam', 'sock', 'phone', 'computer', 'dog', 'day', 'work', 'video', 'life', 'game', 'meme', 'boy', 'number', 'person', 'director', 'firm', 'drill', 'mustache', 'garden', 'fence', 'neighbor', 'outlet', 'table', 'year', 'world', 'hand', 'child', 'eye', 'week', 'point', 'group', 'problem', 'question', 'time', 'work', 'system', 'night', 'home', 'room', 'month', 'book', 'job', 'word', 'description', 'board', 'issue', 'friend', 'car', 'city', 'idea', 'server', 'attack', 'keyboard', 'language', 'level', 'clock', 'war', 'result', 'moment', 'song', 'guitar', 'picture', 'dog', 'pet', 'enemy', 'end', 'album', 'fan', 'look', 'tumblr', 'decision', 'consequence', 'notebook', 'difference', 'game', 'class', 'mirror', 'thought', 'line', 'picture', 'website', 'tag', 'agenda']
+#all nouns
+nounGenerator = ncountnGenerator + countnGenerator
+#verbs
+verbGenerator = ['make', 'hope', 'have', 'get', 'feel', 'like', 'steal', 'glitch', 'do', 'prosper', 'survive', 'stop', 'go', 'recite', 'tell', 'say', 'play', 'keep', 'crash', 'ruin', 'trust', 'use', 'find', 'leave', 'chill', 'practice', 'finish', 'draw', 'write', 'expect', 'spy', 'destroy', 'watch', 'type', 'eat', 'laugh', 'listen', 'pick', 'sell', 'travel', 'ask', 'try', 'call', 'roast', 'yell', 'reblog', 'forget', 'follow', 'load', 'cooperate', 'change', 'heal', 'die', 'post', 'queue']
+#third person pronouns
+thirdPronounGenerator = ['he', 'she', 'it']
+#personal pronouns
+personalPronounGenerator = ['I', 'you', 'we', 'they']
+#object pronouns
+objectPronounGenerator = ['me', 'you', 'him', 'her', 'it', 'them', 'us']
+#conjunctions
+conjunctionGenerator = ['and', 'but', 'or']
+#negatives
+negativeGenerator = ['', ' not']
+#adjectives
+adjectiveGenerator = ['bad', 'late', 'official', 'happy', 'educational', 'entire', 'random', 'fun', 'stupid', 'serious', 'better', 'pretty', 'boring', 'nice', 'problematic', 'real', 'scary', 'sorry', 'normal', 'perfect', 'strange', 'mysterious', 'small', 'large', 'good', 'new', 'long', 'great', 'old', 'big', 'important', 'professional', 'specific', 'ready', 'amazing', 'expensive']
+#dependent sentence conjunctions
+dependentGenerator = ['when', 'because']
