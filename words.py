a84e77f33f0cc1dbc814971167469e4843406061
Nicolas Bravo <nicobravo1@hotmail.com>
2017-01-19 13:09:29 -0500

    Alphabetical order

--- words.py
+++ words.py
@@ -1,24 +1,22 @@
 #noncount nouns
 ncountnGenerator = ['aesthetic', 'cost', 'food', 'history', 'information', 'light', 'money', 'music', 'Outlast', 'power', 'sleep', 'spam', 'storage', 'tennis', 'Tumblr', 'water', '2017']
 #count nouns
-countnGenerator = ['result', 'moment', 'song', 'guitar', 'picture', 'dog', 'pet', 'enemy', 'end', 'album', 'fan', 'look', 'decision', 'consequence', 'notebook', 'difference', 'game', 'class', 'mirror', 'thought', 'line', 'picture', 'website', 'tag', 'agenda', 'volleyball']
-
-countnGenerator = ['attack', 'backpack', 'board', 'book', 'boy', 'car', 'child', 'city', 'clock', 'computer', 'day', 'description', 'diary', 'director', 'dog', 'drill', 'exam', 'eye', 'fence', 'friend', 'game', 'garden', 'girl', 'group', 'hand', 'home', 'idea', 'issue', 'job', 'keyboard', 'language', 'laptop', 'level', 'life', 'meme', 'month', 'movie', 'night', 'number', 'neighbor', 'outlet', 'paper', 'person', 'phone', 'point', 'problem', 'program', 'question', 'remote', 'room', 'server', 'sheet', 'shoe', 'sock', 'system', 'table', 'time', 'video', 'war', 'week', 'word', 'work', 'world', 'year']
+countnGenerator = ['agenda', 'album', 'attack', 'backpack', 'board', 'book', 'boy', 'car', 'child', 'city', 'class', 'clock', 'computer', 'day', 'decision', 'description', 'diary', 'director', 'dog', 'drill', 'end', 'enemy', 'exam', 'eye', 'fan', 'fence', 'friend', 'game', 'garden', 'girl', 'group', 'guitar', 'hand', 'home', 'idea', 'issue', 'job', 'keyboard', 'language', 'laptop', 'level', 'life', 'line', 'look', 'meme', 'mirror', 'moment', 'month', 'movie', 'night', 'number', 'neighbor', 'outlet', 'paper', 'person', 'pet', 'phone', 'picture', 'point', 'problem', 'program', 'question', 'remote', 'result', 'room', 'server', 'sheet', 'shoe', 'sock', 'song', 'system', 'table', 'tag', 'thought', 'time', 'video', 'volleyball', 'war', 'website', 'week', 'word', 'work', 'world', 'year']
 #all nouns
 nounGenerator = ncountnGenerator + countnGenerator
 #verbs
-verbGenerator = ['make', 'hope', 'have', 'get', 'feel', 'like', 'steal', 'glitch', 'do', 'prosper', 'survive', 'stop', 'go', 'recite', 'tell', 'say', 'play', 'keep', 'crash', 'ruin', 'trust', 'use', 'find', 'leave', 'chill', 'practice', 'finish', 'draw', 'write', 'expect', 'spy', 'destroy', 'watch', 'type', 'eat', 'laugh', 'listen', 'pick', 'sell', 'travel', 'ask', 'try', 'call', 'roast', 'yell', 'reblog', 'forget', 'follow', 'load', 'cooperate', 'change', 'heal', 'die', 'post', 'queue']
+verbGenerator = ['ask', 'call', 'change', 'chill', 'cooperate', 'crash', 'destroy', 'die', 'do', 'draw', 'eat', 'expect', 'feel', 'find', 'finish', 'follow', 'forget', 'get', 'glitch', 'go', 'heal', 'have', 'hope', 'keep', 'laugh', 'leave', 'like', 'listen', 'load', 'make', 'pick', 'play', 'post', 'practice', 'queue', 'reblog', 'roast', 'ruin', 'say', 'sell', 'spy', 'steal', 'stop', 'survive', 'tag', 'tell', 'travel', 'trust', 'try', 'type', 'use', 'watch', 'write', 'yell']
 #third person pronouns
-thirdPronounGenerator = ['he', 'she', 'it']
+thirdPronounGenerator = ['he', 'it', 'she']
 #personal pronouns
-personalPronounGenerator = ['I', 'you', 'we', 'they']
+personalPronounGenerator = ['I', 'they', 'we', 'you']
 #object pronouns
-objectPronounGenerator = ['me', 'you', 'him', 'her', 'it', 'them', 'us']
+objectPronounGenerator = ['her', 'him', 'it', 'me', 'them', 'us', 'you']
 #conjunctions
 conjunctionGenerator = ['and', 'but', 'or']
 #negatives
 negativeGenerator = ['', ' not']
 #adjectives
-adjectiveGenerator = ['bad', 'late', 'official', 'happy', 'educational', 'entire', 'random', 'fun', 'stupid', 'serious', 'better', 'pretty', 'boring', 'nice', 'problematic', 'real', 'scary', 'sorry', 'normal', 'perfect', 'strange', 'mysterious', 'small', 'large', 'good', 'new', 'long', 'great', 'old', 'big', 'important', 'professional', 'specific', 'ready', 'amazing', 'expensive']
+adjectiveGenerator = ['amazing', 'bad', 'better', 'big', 'boring', 'educational', 'expensive', 'fun', 'good', 'great', 'happy', 'important', 'large', 'late', 'long', 'mysterious', 'new', 'nice', 'normal', 'official', 'old', 'perfect', 'pretty', 'problematic', 'professional', 'ready', 'real', 'scary', 'serious', 'small', 'sorry', 'specific', 'strange', 'stupid']
 #dependent sentence conjunctions
-dependentGenerator = ['when', 'because']
+dependentGenerator = ['because', 'when']
